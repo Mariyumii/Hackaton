cf91980de5f574b3a354bd606aaa36018b349f45
Marcelo <mariyumiyokomizo@gmail.com>
2025-05-17 22:27:10 -0300

    Initial commit

---
+++ meu-app-react/src/pages/Home.jsx
@@ -0,0 +1,30 @@
+import { useEffect, useState } from 'react';
+import { supabase } from '../lib/supabaseClient';
+
+export default function Home() {
+  const [dados, setDados] = useState([]);
+
+  useEffect(() => {
+    async function carregarDados() {
+      const { data, error } = await supabase.from('produtos').select('*');
+      if (error) {
+        console.error(error);
+      } else {
+        setDados(data);
+      }
+    }
+
+    carregarDados();
+  }, []);
+
+  return (
+    <div>
+      <h1>Home</h1>
+      <ul>
+        {dados.map((item, index) => (
+          <li key={index}>{JSON.stringify(item)}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
